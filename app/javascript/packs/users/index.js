7a1e06f8963144d3a1f614ab4f454b37f6840100
pipopotamasu <ec0156hx@gmail.com>
2018-01-29 07:13:14 +0900

    compare to origin axios

--- app/javascript/packs/users/index.js
+++ app/javascript/packs/users/index.js
@@ -1,5 +1,10 @@
-import axios_on_rails from 'axios-on-rails'
+// import axios_on_rails from 'axios-on-rails'
+import axios from 'axios'
 
-axios_on_rails.post('users', { hoge: 1 }).then((res) => {
+// axios_on_rails.post('users', { hoge: 1 }).then((res) => {
+//   console.log(res)
+// })
+
+axios.post('users', { hoge: 1 }).then((res) => {
   console.log(res)
 })
